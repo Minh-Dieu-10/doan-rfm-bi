7e716f667a0d9a72bb9b22c58c2a2826cb7b65b9
Dieu <minhdieu6605@gmail.com>
2026-07-05 04:29:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ else:
                         batch_size = 1000
                         for i in range(0, len(records), batch_size):
                             batch = records[i:i+batch_size]
-                            supabase.table("Dim_Customer").upsert(batch).execute()
+                            supabase.table('"Dim_Customer"').upsert(batch).execute()
 
                         # LƯU KẾT QUẢ VÀO BỘ NHỚ TẠM SAU KHI THÀNH CÔNG
                         st.session_state.etl_success = True
