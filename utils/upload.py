e413697e844ac65dbc8d9f268d2ee0e9333fa0e3
Dieu <minhdieu6605@gmail.com>
2026-07-09 22:53:14 +0700

    Update upload.py

--- utils/upload.py
+++ utils/upload.py
@@ -1,8 +1,18 @@
-
 from utils.db import supabase
 
-def upload_table(df, table_name):
+def upload_segments(df):
 
-    data = df.to_dict("records")
+    supabase.table("segments").delete().neq("CustomerID",0).execute()
 
-    supabase.table(table_name).insert(data).execute()
+    data=df.to_dict("records")
+
+    supabase.table("segments").insert(data).execute()
+
+
+def upload_recommend(df):
+
+    supabase.table("recommendations").delete().neq("id",0).execute()
+
+    data=df.to_dict("records")
+
+    supabase.table("recommendations").insert(data).execute()
