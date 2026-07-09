d2b275c7258ecd96773c3813ea3551f74e812563
Dieu <minhdieu6605@gmail.com>
2026-07-09 22:23:40 +0700

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 
-SUPABASE_URL="YOUR_URL"
-SUPABASE_KEY="YOUR_KEY"
+SUPABASE_URL = "https://cylljidqxzublpipypcu.supabase.co"
+SUPABASE_KEY = "sb_publishable_3DEP2QBBrM7uT0AZmj4Wpw_9fcnn8h6"
 
 supabase=create_client(
     SUPABASE_URL,
