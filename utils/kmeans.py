5b4db0c217498cabbd2507c616ae772251435079
Dieu <minhdieu6605@gmail.com>
2026-07-09 22:47:27 +0700

    Update kmeans.py

--- utils/kmeans.py
+++ utils/kmeans.py
@@ -1,1 +1,20 @@
 
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+def segment_customer(rfm):
+
+    scaler = StandardScaler()
+
+    X = scaler.fit_transform(
+        rfm[["Recency","Frequency","Monetary"]]
+    )
+
+    model = KMeans(
+        n_clusters=4,
+        random_state=42
+    )
+
+    rfm["Cluster"] = model.fit_predict(X)
+
+    return rfm
