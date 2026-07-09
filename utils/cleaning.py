5f6fc1ac46c7675494df79744a700427c11517c8
Dieu <minhdieu6605@gmail.com>
2026-07-09 22:46:37 +0700

    Update cleaning.py

--- utils/cleaning.py
+++ utils/cleaning.py
@@ -1,1 +1,22 @@
 
+import pandas as pd
+
+def clean_data(df):
+
+    # Xóa dòng thiếu CustomerID
+    df = df.dropna(subset=["CustomerID"])
+
+    # Chỉ giữ giao dịch hợp lệ
+    df = df[df["Quantity"] > 0]
+    df = df[df["UnitPrice"] > 0]
+
+    # Đổi kiểu dữ liệu
+    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+
+    # CustomerID về int
+    df["CustomerID"] = df["CustomerID"].astype(int)
+
+    # Thành tiền
+    df["TotalPrice"] = df["Quantity"] * df["UnitPrice"]
+
+    return df
