4ed20d0521869c01b18ef0d0c45e459c7026d764
Dieu <minhdieu6605@gmail.com>
2026-07-09 22:47:10 +0700

    Update rfm.py

--- utils/rfm.py
+++ utils/rfm.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+def create_rfm(df):
+
+    reference_date = df["InvoiceDate"].max() + pd.Timedelta(days=1)
+
+    rfm = df.groupby("CustomerID").agg({
+
+        "InvoiceDate": lambda x: (reference_date - x.max()).days,
+
+        "InvoiceNo": "nunique",
+
+        "TotalPrice": "sum"
+
+    })
+
+    rfm.columns = ["Recency", "Frequency", "Monetary"]
+
+    return rfm.reset_index()
